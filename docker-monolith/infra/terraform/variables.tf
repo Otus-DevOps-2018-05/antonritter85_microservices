5e5509b3c0a9a29b5547e92fba078bafe50d80b1
Anton Ritter <antonritter85@gmail.com>
2018-08-16 17:13:39 +0300

    Homework gitlab-ci-1: add infrastructure

--- docker-monolith/infra/terraform/variables.tf
+++ docker-monolith/infra/terraform/variables.tf
@@ -16,6 +16,11 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
+variable disk_size {
+  description = "Instance disk size "
+  default     = "10"
+}
+
 variable disk_image {
   description = "Disk image"
 }
